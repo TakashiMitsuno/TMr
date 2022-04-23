cd7d6ed98d866cbd2bbc243de7298202c470827a
TakashiMitsuno <takashimitsuno1@gmail.com>
2022-04-23 16:32:42 +0900

    8-4 課題②

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}', 'PostController@show');
+
